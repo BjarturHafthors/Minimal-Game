c824a5c5388049bb2385474b9a0d1a84041347e4
SwagMal <bjarturhafthors@hotmail.com>
2017-02-05 18:37:48 +0000

    Player movement

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        /*Resolution[] resolutions = Screen.resolutions;
+        foreach (Resolution res in resolutions)
+        {
+            print(res.width + "x" + res.height);
+        }*/
+        Screen.SetResolution(1920, 1080, true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody2D rb2d;
+    public float speed;
+    public float RotateSpeed;
+    private Vector2 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+            transform.Rotate(new Vector3(0, 0, 1) * -RotateSpeed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+            transform.Rotate(new Vector3(0, 0, 1) * RotateSpeed * Time.deltaTime);
+    }
+
+    // Update is called once per frame right before physics happen
+    void FixedUpdate () {
+        //Store the current vertical input in the float moveVertical.
+        float move = Input.GetAxis("Vertical");
+
+        rb2d.velocity = new Vector2(transform.up.x, transform.up.y) * speed * move;
+    }
+}

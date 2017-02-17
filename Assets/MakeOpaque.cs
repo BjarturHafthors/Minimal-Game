c36979fb36bf75de960c84d8eabe7b4ee5e37187
SwagMal <bjarturhafthors@hotmail.com>
2017-02-17 17:52:36 +0000

    Menu Progress

---
+++ Assets/MakeOpaque.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeOpaque : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .2f);
+    }
+}

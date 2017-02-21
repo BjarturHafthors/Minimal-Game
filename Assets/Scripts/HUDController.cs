e709d1e9465804b9f0cbc24f4ccd54df08d08e8a
SwagMal <bjarturhafthors@hotmail.com>
2017-02-21 05:16:40 +0000

    New background and survival timer

--- Assets/Scripts/HUDController.cs
+++ Assets/Scripts/HUDController.cs
@@ -19,5 +19,6 @@ public class HUDController : MonoBehaviour {
     void OnGUI()
     {
         GUI.Label(new Rect(10, 10, 100, 50), "Score: " + player.GetComponent<PlayerController>().score);
+        GUI.Label(new Rect(Screen.width/2-75, 10, 150, 50), "Time alive: " + Time.time.ToString("0.0"));
     }
 }

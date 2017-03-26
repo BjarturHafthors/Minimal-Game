feb967c1f9bbed45d6b992542f7597f421702bf5
SwagMal <bjarturhafthors@hotmail.com>
2017-03-26 00:41:39 +0000

    difficulty

---
+++ Assets/Scripts/Menus/ButtonManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	[SerializeField]
+	public Transform mainMenu;
+	[SerializeField]
+	public Transform controlsMenu;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.M)) {
+			Mute();
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Back();
+		} 
+	}
+
+	public void Back() {
+		if (controlsMenu.gameObject.activeInHierarchy == true) {
+			controlsMenu.gameObject.SetActive (false);
+			mainMenu.gameObject.SetActive (true);
+		}
+	}
+
+	public void NewGame() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void Mute() {
+		BackgroundMusic.Instance.Mute (); 
+	}
+
+	public void Controls() {
+		if (controlsMenu.gameObject.activeInHierarchy == false) {
+			mainMenu.gameObject.SetActive (false);
+			controlsMenu.gameObject.SetActive (true);
+		}
+	}
+
+	public void ExitGame() {
+
+		Application.Quit();
+		//UnityEditor.EditorApplication.isPlaying = false;
+	}	
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public Text health;
+    private float time;
+    public Text timeSurvived;
+    public PlayerController player;
+    public SurivalTime survival;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        health.text = System.String.Format("{0:d}", "Health: " + player.health);
+        time = survival.getTimeSurvived();
+
+        if (Mathf.Floor(time / 60) >= 1)
+        {
+            timeSurvived.text = System.String.Format("{0:d}", "Time: " + Mathf.Floor(time / 60) + ":" + Mathf.Floor(time % 60).ToString("00"));
+        }
+        else
+        {
+            timeSurvived.text = System.String.Format("{0:d}", "Time: 00:" + Mathf.Floor(time % 60).ToString("00"));
+        }
+    }
+}
